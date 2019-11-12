7635e5438ff6277922c3f3f307d61e56b7652e09
dimkasta <dimkasta@yahoo.gr>
2019-11-13 01:40:49 +0200

    Update pcinfo.sh

--- pcinfo.sh
+++ pcinfo.sh
@@ -1,21 +1,21 @@
 
 get_user(){
-    echo -e "$USER"
+    echo -e "$USER "
 }
 
 get_ip(){
-    format "$(ip route get 1 | tr -s ' ' | cut -d' ' -f7)" lightblue    
+    format "$(ip route get 1 | tr -s ' ' | cut -d' ' -f7) " lightblue    
 }
 
 get_directory(){
-    format $(dirs) green
+    format "$(dirs) " green
 }
 
 get_fqdn(){
-    format "$(hostname -f)" bold
+    format "$(hostname -f) " bold
 }
 
 get_ssh(){
-    format "$(if [[ $(who am i) =~ \([-a-zA-Z0-9\.]+\)$ ]] ; then echo '(SSH)'; fi)" lightblue
+    format "$(if [[ $(who am i) =~ \([-a-zA-Z0-9\.]+\)$ ]] ; then echo '(SSH)'; fi) " lightblue
 #    echo -e "$lightblue$(if [[ $(who am i) =~ \([-a-zA-Z0-9\.]+\)$ ]] ; then echo '(SSH)'; fi)$nocolor "
 }
