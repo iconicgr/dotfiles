0a3e47e0f2dd91998517dfb86ebec30b14ca5f09
dimkasta <dimkasta@yahoo.gr>
2023-10-05 21:55:26 +0300

    added nvim lua
    	new file:   init.lua

---
+++ init.lua
@@ -0,0 +1,100 @@
+-- Editor Settings 
+
+-- Disable mouse so that copy paste works on SSH
+vim.opt.mouse = ""
+
+-- Set leader to space. TODO: WHY?
+vim.g.mapleader = " "
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+-- vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+
+-- Keymaps
+-- Moving
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>r", [[*ggncgn]])
+
+-- Plugins
+
+-- Use packer manager
+vim.cmd [[packadd packer.nvim]]
+
+require('packer').startup(function(use)
+	-- Packer
+	use 'wbthomason/packer.nvim'
+
+	-- Telescope
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.3',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	-- Github Theme
+	use({
+		'projekt0n/github-nvim-theme',
+		config = function()
+			require('github-theme').setup({
+				-- ...
+			})
+
+			vim.cmd('colorscheme github_dark')
+		end
+	})
+
+	-- Treesitter
+	use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+end
+)
+
+-- Telescope Keybinds
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+-- Treesitter setup
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = "all",
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+    
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+
