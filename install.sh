3b920c2ac56a005d194fef9c4f66d6436fb3803e
dimkasta <d.kastaniotis@iconic.gr>
2023-10-13 15:14:11 +0300

    conf micro as standard editor
    ammended

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 echo source /home/$USER/dotfiles/.bashrc >> ~/.bashrc
 echo source /home/$USER/dotfiles/.bash_aliases >> ~/.bashrc
+
+mkdir -p ~/.config/micro
+ln -s ~/dotfiles/micro_settings.json ~/.config/micro/settings.json
