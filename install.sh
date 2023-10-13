18c5e99608b60086fc04a9409f2739e151293241
Dimitris Kastaniotis <dimkasta@yahoo.gr>
2023-10-13 23:10:46 +0300

    removed old micro setup

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 echo source /home/$USER/dotfiles/bash >> ~/.bashrc
 echo source /home/$USER/dotfiles/aliases >> ~/.bashrc
 
-mkdir -p ~/.config/micro
-ln -s ~/dotfiles/micro_settings.json ~/.config/micro/settings.json
