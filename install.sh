c863fae9b04db0cde7e4c497cd48e0b19f0cda10
dimkasta <dimkasta@nas.net.iconic.gr>
2023-10-13 15:07:03 +0300

    removed nvim

--- install.sh
+++ install.sh
@@ -1,4 +1,2 @@
-#echo source /home/$USER/dotfiles/.bashrc >> ~/.bashrc
-#echo source /home/$USER/dotfiles/.bash_aliases >> ~/.bashrc
-
-ln -s /home/$USER/dotfiles/init.lua /home/$USER/.config/nvim/init.lua
+echo source /home/$USER/dotfiles/.bashrc >> ~/.bashrc
+echo source /home/$USER/dotfiles/.bash_aliases >> ~/.bashrc
