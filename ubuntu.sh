dd6439bc556c8a350804ca0153259408c482fcc3
dimkasta <dimkasta@yahoo.gr>
2025-04-08 07:07:41 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+sudo apt install gnome-tweaks
+sudo apt install curl
+sudo apt install git
+sudo apt install micro
+
+## PHP
+
+sudo apt install php
+sudo apt install php-dom
+sudo apt install php-xml
+sudo apt install php-intl
+sudo apt install php-curl
+sudo apt install php-gd
+sudo apt install php-imagick
+sudo apt install php-mysqli
+
+## Composer
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+## Docker
+
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Add Docker's official GPG key:
